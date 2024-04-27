f1086ac777b96e71be41faada160d1c782f8999a
fakhrimanai <164647425+fakhrimanai@users.noreply.github.com>
2024-04-27 14:22:06 +0100

    Add files via upload

--- panier.js
+++ panier.js
@@ -1,77 +1,73 @@
-var compteur=function(a,b,c){
-	var counter = 0;
-	const counterValue = document.querySelector(`#${a}`);
-	console.log(counterValue)
-	const incrementBtn = document.querySelector(`#${b}`);
-	const decrementBtn = document.querySelector(`#${c}`);
-	
-	// To increment the value of counter
-	incrementBtn.addEventListener('click', function(){
-		counter++;
-		counterValue.innerHTML = counter;
-	
-	total()
-	
-	});
-	
-	// To decrement the value of counter 
-	decrementBtn.addEventListener('click', () => {
-		counter--;
-		counterValue.innerHTML = counter;
-		if (counterValue.innerHTML<0){counter=0;
-			counterValue.innerHTML=0} 
-	total()
-	
-	});
-	}
+var compteur = function(a, b, c) {
+    var counter = 0;
+    const counterValue = document.querySelector(`#${a}`);
+    // console.log(counterValue)
+    const incrementBtn = document.querySelector(`#${b}`);
+    const decrementBtn = document.querySelector(`#${c}`);
 
-	compteur('counter-value','increment-btn','decrement-btn');
-	compteur('counter-value1','increment-btn1','decrement-btn1');
-	compteur('counter-value2','increment-btn2','decrement-btn2');
+    // To increment the value of counter
+    incrementBtn.addEventListener('click', function() {
+        counter++;
+        counterValue.innerHTML = counter;
 
+        total()
+    });
 
-var trashcan=function(a,b){
-a.addEventListener('click', () =>{
-		 b.remove();
-	}
-)
+    // To decrement the value of counter 
+    decrementBtn.addEventListener('click', () => {
+        counter--;
+        counterValue.innerHTML = counter;
+        if (counterValue.innerHTML < 0) {
+            counter = 0;
+            counterValue.innerHTML = 0
+        }
+
+        total()
+    });
 }
 
-trashcan(trash1,section1);
-trashcan(trash2,section2);
-trashcan(trash3,section3);
+compteur('counter-value', 'increment-btn', 'decrement-btn');
+compteur('counter-value1', 'increment-btn1', 'decrement-btn1');
+compteur('counter-value2', 'increment-btn2', 'decrement-btn2');
 
 
-var heart=function(c){
-	c.addEventListener('click',function(){
-		if(c.style.color!="red"){c.style.color="red";}
-		else{c.style.color="black"}
+var trashcan = function(a, b) {
+    a.addEventListener('click', () => {
+        b.remove();
+        total()
 
-	})
+    })
 }
+
+trashcan(trash1, section1);
+trashcan(trash2, section2);
+trashcan(trash3, section3);
+
+var heart = function(c) {
+    c.addEventListener('click', function() {
+        if (c.style.color != "red") {
+            c.style.color = "red";
+        } else {
+            c.style.color = "black"
+        }
+
+    })
+}
+
 heart(heart1);
 heart(heart2);
 heart(heart3);
 
 
-var total=function(){
-let qte=Array.from(document.querySelectorAll('.qte'))
-let price=Array.from(document.querySelectorAll('.price'))
-let sum=0;
-for(let i=0;i<qte.length;i++){
-sum+=(qte[i].innerHTML)*(price[i].innerHTML);
+var total = function() {
+    let qte = Array.from(document.querySelectorAll('.qte'))
+    let price = Array.from(document.querySelectorAll('.price'))
+    let sum = 0;
+    for (let i = 0; i < qte.length; i++) {
+        sum += (qte[i].innerHTML) * (price[i].innerHTML);
 
+    }
+    document.querySelector('#totall').innerHTML = sum;
 }
-document.querySelector('#totall').innerHTML=sum;
-}
-let price=Array.from(document.querySelectorAll('.price'))
+let price = Array.from(document.querySelectorAll('.price'))
 console.log(price[1].innerHTML);
-
-
-
-
-
-
-
-
-
